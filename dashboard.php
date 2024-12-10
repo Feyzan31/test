2c25933cc3b49f5d6ccd73836e14ac4b3dc436b8
Feyzan31 <161055249+Feyzan31@users.noreply.github.com>
2024-12-10 22:12:07 +0100

    Add files via upload

---
+++ dashboard.php
@@ -0,0 +1,61 @@
+<?php
+    include 'entete.php';
+?>
+
+<div class="contenu-accueil">
+    <div class="overview-boxes">
+        <div class="box">
+            <div class="right-side">
+                <div class="box-topic">Achats matériels</div>
+                <div class="number">31</div>
+                <div class="indicator">
+                    <i class="bx bx-up-arrow-alt"></i>
+                    <span class="text">Depuis hier</span>
+                </div>
+            </div>
+            <i class="bx bx-cart-alt cart"></i>
+        </div>
+    </div>
+    <div class="Etats-boxes">
+        <div class="affectation-recente box">
+            <div class="title">Affectations des matériels</div>
+            <div class="affectation-details">
+                <ul class="details">
+                    <li class="topic">Date</li>
+                    <li><a href="#">15 Mars 2024</a></li>
+                    <li><a href="#">15 Mars 2024</a></li>
+                    <li><a href="#">19 Mars 2024</a></li>
+                    <li><a href="#">19 Mars 2024</a></li>
+                    <li><a href="#">01 Avril 2024</a></li>
+                    <li><a href="#">02 Avril 2024</a></li>
+                </ul>
+                <ul class="details">
+                    <li class="topic">Département</li>
+                    <li><a href="#">Scolarité</a></li>
+                    <li><a href="#">Amphi 3</a></li>
+                    <li><a href="#">Amphi 3</a></li>
+                    <li><a href="#">Batiment A</a></li>
+                    <li><a href="#">Amphi 4</a></li>
+                    <li><a href="#">Batiment B</a></li>
+                </ul>
+                <ul class="details">
+                    <li class="topic">Matériel</li>
+                    <li><a href="#">Imprimante</a></li>
+                    <li><a href="#">Serveur</a></li>
+                    <li><a href="#">Projecteur</a></li>
+                    <li><a href="#">Plaque de cuisson</a></li>
+                    <li><a href="#">Bureau</a></li>
+                    <li><a href="#">Disjoncteur</a></li>
+                </ul>
+            </div>
+            <div class="button">
+                <a href="#">Voir Tout</a>
+            </div>
+        </div>
+    </div>
+</div>
+</section>
+
+<?php
+    include 'pied.php';
+?>
